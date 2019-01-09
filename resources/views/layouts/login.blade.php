d00dbd4f8488fbc618a632ede91c790cd6448e27
85mukeshyadav <85mukeshyadav@gmail.com>
2019-01-10 00:47:16 +0530

    my-first-commit

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>School ERP</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico"> -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+    <link href="{{ asset('public/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/owl.carousel.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/owl.theme.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/owl.transitions.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/animate.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/normalize.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/scrollbar/jquery.mCustomScrollbar.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/wave/waves.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/notika-custom-icon.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/css/responsive.css') }}" rel="stylesheet">
+    
+    
+    <script type="text/javascript" src="{{ asset('public/js/vendor/modernizr-2.8.3.min.js') }}"></script>
+</head>
+
+<body>
+    @yield('content')
+
+    <script type="text/javascript" src="{{ asset('public/js/vendor/modernizr-2.8.3.min.js') }}"></script>
+    
+    <script type="text/javascript" src="{{ asset('public/js/vendor/jquery-1.12.4.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/wow.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/jquery-price-slider.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/owl.carousel.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/jquery.scrollUp.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/meanmenu/jquery.meanmenu.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/counterup/jquery.counterup.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/counterup/waypoints.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/counterup/counterup-active.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/scrollbar/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/sparkline/jquery.sparkline.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/sparkline/sparkline-active.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/flot/jquery.flot.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/flot/jquery.flot.resize.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/flot/flot-active.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/knob/jquery.knob.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/knob/jquery.appear.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/knob/knob-active.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/chat/jquery.chat.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/wave/waves.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/wave/wave-active.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/icheck/icheck.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/icheck/icheck-active.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/todo/jquery.todo.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/login/login-action.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/plugins.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('public/js/main.js') }}"></script>
+</body>
+
+</html>
